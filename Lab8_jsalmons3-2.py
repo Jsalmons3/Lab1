447dc267265d0f2c91bfd2ed86e1bb748d1c2b7b
Jeffrey Salmons <jsalmons3@studnet.cscc.edu>
2026-06-18 21:39:04 -0400

    imported calc_perimeter from rectangle and made the elif statements for the rectangle

--- Lab8_jsalmons3-2.py
+++ Lab8_jsalmons3-2.py
@@ -8,6 +8,7 @@ This program creates a menu for the user to calculate the area and perimeter/cir
 from circle import calc_area as circle_area
 from circle import calc_circumference
 from rectangle import calc_area as rectangle_area
+from rectangle import calc_perimeter
 
 # Aliases are necessary when importing modules because they both have the same function names 
 # and this will allow us to use specified functions from each module without any mistakes.
@@ -33,3 +34,15 @@ while True:
         radius = float(input("Enter the radius of the circle: "))
         circumference = calc_circumference(radius)
         print(f"The circumference of the circle is {circumference:.3f}")
+    
+    elif choice == 3:
+        width = float(input("Enter the width of the rectangle: "))
+        height = float(input("Enter the height of the rectangle: "))
+        area = rectangle_area(width, height)
+        print(f"The area of the rectangle is {area}")
+
+    elif choice == 4:
+        width = float(input("Enter the width of the rectangle: "))
+        height = float(input("Enter the height of the rectangle: "))
+        perimeter = calc_perimeter(width, height)
+        print(f"The perimeter of the rectangle is {perimeter}")
