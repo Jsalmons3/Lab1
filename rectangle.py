d625d0990d0bf2d2a8c29edad2295832b9d8d3f1
Jeffrey Salmons <jsalmons3@studnet.cscc.edu>
2026-06-18 20:31:51 -0400

    made the calculations for the area and circumference of a rectangle

--- rectangle.py
+++ rectangle.py
@@ -4,3 +4,11 @@ Jeffrey Salmons
 This program makes a forumla for the area and perimeter of a rectangle.
 6/18/2026
 """
+
+def calc_area(width, height):
+    area = width * height
+    return area
+
+def calc_perimeter(width, height):
+    perimeter = 2 * (width + height)
+    return perimeter
