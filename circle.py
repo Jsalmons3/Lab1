64c81dcf88c60a08e456deda24614cf14d75f14c
Jeffrey Salmons <jsalmons3@studnet.cscc.edu>
2026-06-18 20:29:11 -0400

    made the calculations for the area and circumference of a circle

--- circle.py
+++ circle.py
@@ -4,3 +4,11 @@ Jeffrey Salmons
 This program makes a forumla for the area and circumference of a circle.
 6/18/2026
 """
+
+def calc_area(radius):
+    area = 3.14 * radius ** 2
+    return area
+
+def calc_circumference(radius):
+    circumference = 2 * 3.14 * radius
+    return circumference
